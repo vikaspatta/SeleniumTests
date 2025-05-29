a96aafe6efcc132c1969eb756e5085b9535505fb
Vikas Patta <vikaspatta@gmail.com>
2025-05-29 21:25:32 +0530

    adding feature file

---
+++ src/test/java/com/example/steps/StepDefinitions.java
@@ -0,0 +1,28 @@
+
+package com.example.steps;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class StepDefinitions {
+
+    @Given("I launch chrome browser")
+    public void launchChrome() {
+
+    }
+    @And("I navigate to google page")
+    public void openGoogle() {
+
+    }
+    @Then("I verify the title of the page")
+    public void verifyTitlePage() {
+
+    }
+    @And("I close the browser")
+    public void closeBrowser() {
+
+    }
+
+
+
+}
